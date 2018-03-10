6b66bea3f6f23641d25fd9191b3df9320b206de4
Alex Howard <thezanke@gmail.com>
2018-03-10 09:57:24 -0500

    finish card, more or less

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,11 @@ import React, { Fragment } from 'react';
 import styled, { injectGlobal } from 'styled-components';
 
 import Maze from './Maze';
+import { LinkedinIcon, GithubIcon, TwitterIcon } from './icons';
+
+const GRAVATAR_SIZE = 128;
+const GRAVATAR_URL = 'http://gravatar.com/avatar/f16320597c08a8462094030bcee31207?size=' + GRAVATAR_SIZE;
+const ICON_SIZE = 32;
 
 injectGlobal`
   html, body {
@@ -15,10 +20,6 @@ injectGlobal`
   body {
     background-color: black;
     color: white;
-  }
-
-  .text-center {
-    text-align: center;
   }
 `;
 
@@ -39,22 +40,81 @@ const RootLayout = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  > div {
+    flex: 0 1;
+  }
 `;
 
 const PersonalInfo = styled.main`
-  flex: 0 1;
+  display: flex;
 
-  > * {
-    margin: 0;
-    padding: 0;
+  svg {
+    fill: #ffffff;
   }
+`;
+
+const Content = styled.div`
+  height: 100%;
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
   h1 {
+    margin: 0;
+    padding: 0;
     font-family: Helvetica, sans-serif;
     font-weight: bold;
     font-size: 3rem;
+    vertical-align: top;
+  }
+
+  em {
+    color: #e3e3e3;
+  }
+
+  > div:last-child {
+    flex: 1;
   }
 `;
+
+const GravatarRoot = styled.div`
+  display: inline-block;
+  width: ${GRAVATAR_SIZE}px;
+  height: ${GRAVATAR_SIZE}px;
+  border-radius: ${GRAVATAR_SIZE / 6}px;
+  overflow: hidden;
+`;
+
+const Gravatar = () => (
+  <GravatarRoot>
+    <img src={GRAVATAR_URL} alt="gravatar" />
+  </GravatarRoot>
+);
+
+const SocialLinksRoot = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  > *:not(:first-child) {
+    margin-left: 0.5em;
+  }
+`;
+
+const SocialLinks = () => (
+  <SocialLinksRoot>
+    <a href="https://www.github.com/thezanke" target="_blank" rel="noopener noreferrer">
+      <GithubIcon size={ICON_SIZE} />
+    </a>
+    <a href="https://www.twitter.com/thezanke" target="_blank" rel="noopener noreferrer">
+      <TwitterIcon size={ICON_SIZE} />
+    </a>
+    <a href="https://www.linkedin.com/in/alex-howard-9597a957/" target="_blank" rel="noopener noreferrer">
+      <LinkedinIcon size={ICON_SIZE} />
+    </a>
+  </SocialLinksRoot>
+);
 
 const App = () => (
   <Fragment>
@@ -62,13 +122,24 @@ const App = () => (
       <Maze />
     </MazeContainer>
     <RootLayout>
-      <PersonalInfo>
-        <div className="text-center">
-          <img src="http://gravatar.com/avatar/f16320597c08a8462094030bcee31207?size=100" alt="gravatar" />
-        </div>
-        <h1>Alex Howard</h1>
-        <em>Giant Software Developer</em>
-      </PersonalInfo>
+      <div>
+        <PersonalInfo>
+          <div>
+            <Gravatar />
+          </div>
+          <div>
+            <Content>
+              <div>
+                <h1>Alex Howard</h1>
+                <em>Giant Software Developer</em>
+              </div>
+              <div>
+                <SocialLinks />
+              </div>
+            </Content>
+          </div>
+        </PersonalInfo>
+      </div>
     </RootLayout>
   </Fragment>
 );
