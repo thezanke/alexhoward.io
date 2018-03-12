fa86db64e26dbdc46a2b6b8d39724ed81ee67968
Alex Howard <thezanke@gmail.com>
2018-03-11 23:22:02 -0400

    number decorations

--- src/components/PowerMode.js
+++ src/components/PowerMode.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { createPortal } from 'react-dom';
 import styled from 'styled-components';
+import noop from 'lodash/noop';
+
+const UNLOCK_COUNT = 42;
 
 const PowerModeRoot = styled.div`
   position: fixed;
@@ -15,19 +18,50 @@ const Count = styled.div`
   color: #eee;
   font-weight: 400;
   text-shadow: 1px 1px 1px black;
+  transition: font-size 300ms;
+
+  ${props =>
+    props.unlockable &&
+    `
+    font-weight: bold;
+    font-size: 1.2rem;
+    
+    &:hover {
+      font-size: 1.5rem;
+    }
+  `};
+
+  ${props => props.unlocked && `color: yellow;`}
 `;
 
 // TODO: ball mode
 
-const PowerMode = ({ count }) => {
-  if (count < 10) return null;
+class PowerMode extends PureComponent {
+  state = { unlocked: false };
 
-  return createPortal(
-    <PowerModeRoot>
-      <Count>{count}</Count>
-    </PowerModeRoot>,
-    document.getElementById('root')
-  );
-};
+  unlock = () => this.setState({ unlocked: true });
+
+  render() {
+    const { count } = this.props;
+    const { unlocked } = this.state;
+
+    if (count < 10) return null;
+
+    const unlockable = count === UNLOCK_COUNT;
+
+    return createPortal(
+      <PowerModeRoot>
+        <Count
+          onClick={unlockable ? this.unlock : noop}
+          unlockable={!unlocked && unlockable}
+          unlocked={unlocked}
+        >
+          {count}
+        </Count>
+      </PowerModeRoot>,
+      document.getElementById('root')
+    );
+  }
+}
 
 export default PowerMode;
